00e54208e839487888fd7ec2ec5007f15ec2c61c
nntcuong <thecuongyt3htk@gmail.com>
2024-03-12 23:20:04 +0700

    dfd

--- backend/src/routes/UserRoute.ts
+++ backend/src/routes/UserRoute.ts
@@ -1,9 +1,8 @@
 import  express  from "express";
 import UserController from "../controllers/UserController";
 import { jwtCheck,jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 const router=express.Router();
-
-
 router.post("/",jwtCheck,UserController.createUser)
-router.put("/",jwtCheck, jwtParse,UserController.updateUser)
+router.put("/",jwtCheck, jwtParse,validateMyUserRequest,UserController.updateUser)
 export default router
